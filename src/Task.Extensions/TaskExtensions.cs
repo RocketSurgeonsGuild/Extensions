2ff29cbf6772864d79cad3ad3f9d5b7997331e7d
RLittlesII <rodney.littles@gmail.com>
2018-08-13 21:48:23 -0500

    Added Task Extensions

---
+++ src/Task.Extensions/TaskExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Runtime.CompilerServices;
+
+// ReSharper disable once CheckNamespace
+namespace System.Threading.Tasks
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Continues the <see cref="Task"/> return on any <see cref="IScheduler"/> context.
+        /// </summary>
+        /// <param name="this">The this.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">this</exception>
+        public static ConfiguredTaskAwaitable ContinueOnAnyContext(this Task @this)
+        {
+            if (@this == null) throw new ArgumentNullException(nameof(@this));
+            return @this.ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Continues the <see cref="Task"/> return on any <see cref="IScheduler"/> context.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this">The this.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">this</exception>
+        public static ConfiguredTaskAwaitable<T> ContinueOnAnyContext<T>(this Task<T> @this)
+        {
+            if (@this == null) throw new ArgumentNullException(nameof(@this));
+            return @this.ConfigureAwait(false);
+        }
+    }
+}
