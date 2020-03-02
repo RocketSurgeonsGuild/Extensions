ee227fe8eb009f2222322696ba0e0d13a48a1414
David Driscoll <david.driscoll@gmail.com>
2020-03-01 21:20:56 -0500

    Added RealThrottle and Debounce observable methods

---
+++ src/Reactive.Extensions/Debounce.cs
@@ -0,0 +1,117 @@
+﻿using System.Reactive.Concurrency;
+
+namespace System.Reactive.Linq
+{
+    internal class Debounce<T> : ObserverBase<T>, IObserver<Unit>
+    {
+        private readonly object _gate = new object();
+        private T _value;
+        private bool _hasValue;
+        private readonly bool _leading;
+        private readonly bool _trailing;
+        private readonly IScheduler _scheduler;
+        private IDisposable? _serialCancelable;
+        private ulong _id;
+        readonly IObservable<Unit> _notifier;
+        readonly IObserver<T> _destination;
+
+        public Debounce(IObserver<T> destination, IObservable<Unit> notifier, bool leading,
+                        bool trailing, IScheduler scheduler)
+        {
+            _leading = leading;
+            _trailing = trailing;
+            _scheduler = scheduler;
+            _notifier = notifier;
+            _destination = destination;
+            _value = default!;
+        }
+
+        protected override void OnCompletedCore()
+        {
+            if (_trailing)
+            {
+                send();
+            }
+
+            _destination.OnCompleted();
+        }
+
+        protected override void OnErrorCore(Exception error)
+        {
+            _destination.OnError(error);
+        }
+
+        protected override void OnNextCore(T value)
+        {
+            lock (_gate)
+            {
+                _hasValue = true;
+                _value = value;
+                _id = unchecked(_id + 1);
+            }
+
+            if (_serialCancelable == null && _leading)
+            {
+                send();
+            }
+
+            throttle();
+        }
+
+        private void Done()
+        {
+            lock (_gate)
+            {
+                if (_serialCancelable != null)
+                {
+                    _serialCancelable?.Dispose();
+                    _serialCancelable = null;
+                }
+            }
+
+            if (_trailing)
+            {
+                send();
+            }
+        }
+
+        private void throttle()
+        {
+            lock (_gate)
+            {
+                _serialCancelable?.Dispose();
+                _serialCancelable = null;
+                _serialCancelable = _notifier
+                   .ObserveOn(_scheduler)
+                   .SubscribeSafe(this);
+            }
+        }
+
+        private void send()
+        {
+            if (_hasValue)
+            {
+                _destination.OnNext(_value);
+                throttle();
+            }
+
+            _hasValue = false;
+            _value = default!;
+        }
+
+        void IObserver<Unit>.OnCompleted()
+        {
+            Done();
+        }
+
+        void IObserver<Unit>.OnError(Exception error)
+        {
+            OnError(error);
+        }
+
+        void IObserver<Unit>.OnNext(Unit value)
+        {
+            Done();
+        }
+    }
+}
---
+++ test/Extensions.Tests/Observables/RealThrottleTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Reactive.Testing;
+using Rocket.Surgery.Extensions.Testing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Rocket.Surgery.Extensions.Tests.Observables
+{
+    public class RealThrottleTests : LoggerTest
+    {
+        private readonly TestScheduler _scheduler;
+
+        public RealThrottleTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, LogLevel.Information)
+        {
+            _scheduler = new TestScheduler();
+        }
+
+        [Fact]
+        public void Should_Throttle_On_Leading_Edge()
+        {
+            var input = " a-b-c-d-|";
+            var output = "a---c----|";
+            var observable = _scheduler.CreateHotObservable(input);
+
+            var receiver = _scheduler.CreateObserver<char>();
+            observable.RealThrottle(TimeSpan.FromTicks(20), scheduler: _scheduler).Subscribe(receiver);
+            _scheduler.Start();
+
+            receiver.GetMarbles().Should().Be(output);
+        }
+
+        [Fact]
+        public void Should_Throttle_On_Trailing_Edge()
+        {
+            var input = " a-b-c-d-|";
+            var output = "--b--c--d-|";
+            var observable = _scheduler.CreateHotObservable(input);
+
+            var receiver = _scheduler.CreateObserver<char>();
+            observable.RealThrottle(TimeSpan.FromTicks(20), leading: false, trailing: true, scheduler: _scheduler)
+               .Subscribe(receiver);
+            _scheduler.Start();
+
+            receiver.GetMarbles().Should().Be(output);
+        }
+
+        [Fact]
+        public void Should_Ignore_Overlap()
+        {
+            var input = " a-b-c-d-|";
+            var output = "a-b--c--d-|";
+            var observable = _scheduler.CreateHotObservable(input);
+
+            var receiver = _scheduler.CreateObserver<char>();
+            observable.RealThrottle(TimeSpan.FromTicks(5), leading: false, trailing: true, scheduler: _scheduler)
+               .Subscribe(receiver);
+            _scheduler.Start();
+
+            receiver.GetMarbles().Should().Be(output);
+        }
+
+        [Fact]
+        public void Should_Throttle_On_Both_Edges()
+        {
+            var input = " a-b-c-d-|";
+            var output = "a----c---d|";
+            var observable = _scheduler.CreateHotObservable(input);
+
+            var receiver = _scheduler.CreateObserver<char>();
+            observable.RealThrottle(TimeSpan.FromTicks(40), leading: true, trailing: true, scheduler: _scheduler)
+               .Subscribe(receiver);
+            _scheduler.Start();
+
+            receiver.GetMarbles().Should().Be(output);
+        }
+    }
+}
