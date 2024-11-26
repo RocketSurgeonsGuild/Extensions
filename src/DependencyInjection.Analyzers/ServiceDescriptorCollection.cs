b78b35a5066be34630e8255a9f508c4e1a087174
Rocket Understudy <33589210+rsg-bot@users.noreply.github.com>
2024-11-26 10:37:11 -0500

    Automatically linting code

--- src/DependencyInjection.Analyzers/ServiceDescriptorCollection.cs
+++ src/DependencyInjection.Analyzers/ServiceDescriptorCollection.cs
@@ -165,7 +165,7 @@ internal static class ServiceDescriptorCollection
         var asSelf = serviceTypes.ServiceTypeDescriptors.OfType<SelfServiceTypeDescriptor>().Any() || !serviceTypes.ServiceTypeDescriptors.Any();
         var asImplementedInterfaces = serviceTypes.ServiceTypeDescriptors.OfType<ImplementedInterfacesServiceTypeDescriptor>().ToArray();
         var asMatchingInterface = serviceTypes.ServiceTypeDescriptors.OfType<MatchingInterfaceServiceTypeDescriptor>().Any();
-        var asSpecificTypes = serviceTypes.ServiceTypeDescriptors.OfType<CompiledServiceTypeDescriptor>().Select(z => z.Type).Where(z => z is {}).ToArray();
+        var asSpecificTypes = serviceTypes.ServiceTypeDescriptors.OfType<CompiledServiceTypeDescriptor>().Select(z => z.Type).Where(z => z is { }).ToArray();
         var registrationLifetimeAttribute = compilation.GetTypeByMetadataName("Rocket.Surgery.DependencyInjection.RegistrationLifetimeAttribute")!;
 
         var services = new List<InvocationExpressionSyntax>();
