14875da57ec06b9a903f3bc698834d1d6e582d81
David Driscoll <david.driscoll@gmail.com>
2024-11-28 00:07:52 -0500

    Fix/for real (#1345)
    * nested types can be annoying, so lets fix that
    
    * Cleaned up how private classes are loaded

--- src/DependencyInjection.Analyzers/StatementGeneration.cs
+++ src/DependencyInjection.Analyzers/StatementGeneration.cs
@@ -57,7 +57,7 @@ internal static class StatementGeneration
                             )
                            .WithArgumentList(
                                 ArgumentList(
-                                    SingletonSeparatedList(Argument(GetTypeOfExpression(compilation, implementationType)))
+                                    SingletonSeparatedList(Argument(GetTypeOfExpression(compilation, implementationType, serviceType)))
                                 )
                             ),
                         IdentifierName(Helpers.GetGenericDisplayName(serviceType))
@@ -193,44 +193,35 @@ internal static class StatementGeneration
 
     public static ExpressionSyntax GetTypeOfExpression(Compilation compilation, INamedTypeSymbol type)
     {
-        if (type.IsGenericType && !type.IsOpenGenericType())
-        {
-            var result = compilation.IsSymbolAccessibleWithin(type.ConstructUnboundGenericType(), compilation.Assembly);
-            if (!result)
-            {
-                var name = ParseTypeName(type.ConstructUnboundGenericType().ToDisplayString());
-                if (name is GenericNameSyntax genericNameSyntax)
-                {
-                    name = genericNameSyntax.WithTypeArgumentList(
-                        TypeArgumentList(
-                            SeparatedList<TypeSyntax>(
-                                genericNameSyntax.TypeArgumentList.Arguments.Select(_ => OmittedTypeArgument()).ToArray()
-                            )
-                        )
-                    );
-                }
+        if (type.IsGenericType && type.IsOpenGenericType()) return getPrivateType(compilation, type);
 
-                return InvocationExpression(
-                        MemberAccessExpression(
-                            SyntaxKind.SimpleMemberAccessExpression,
-                            TypeOfExpression(name),
-                            IdentifierName("MakeGenericType")
-                        )
+        return !compilation.IsSymbolAccessibleWithin(type, compilation.Assembly) && type.IsGenericType && !type.IsOpenGenericType()
+            ? InvocationExpression(
+                    MemberAccessExpression(
+                        SyntaxKind.SimpleMemberAccessExpression,
+                        getPrivateType(compilation, type.ConstructUnboundGenericType()),
+                        IdentifierName("MakeGenericType")
+                    )
+                )
+               .WithArgumentList(
+                    // ReSharper disable once NullableWarningSuppressionIsUsed
+                    ArgumentList(SeparatedList(type.TypeArguments.Select(t => Argument(getPrivateType(compilation, ( t as INamedTypeSymbol )!)))))
+                )
+            : getPrivateType(compilation, type);
+
+        static ExpressionSyntax getPrivateType(Compilation compilation, INamedTypeSymbol type)
+        {
+            return compilation.IsSymbolAccessibleWithin(type, compilation.Assembly)
+                ? TypeOfExpression(ParseTypeName(Helpers.GetTypeOfName(type)))
+                : InvocationExpression(
+                        MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, GetPrivateAssembly(type.ContainingAssembly), IdentifierName("GetType"))
                     )
                    .WithArgumentList(
                         ArgumentList(
-                            SeparatedList(
-                                type.TypeArguments
-                                    .Select(t => Argument(GetTypeOfExpression(compilation, ( t as INamedTypeSymbol )!)))
-                            )
+                            SingletonSeparatedList(Argument(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal(Helpers.GetFullMetadataName(type)))))
                         )
                     );
-            }
         }
-
-        return compilation.IsSymbolAccessibleWithin(type, compilation.Assembly)
-            ? TypeOfExpression(ParseTypeName(Helpers.GetTypeOfName(type)))
-            : GetPrivateType(compilation, type);
     }
 
 
@@ -268,26 +259,6 @@ internal static class StatementGeneration
         }
 
         return GetTypeOfExpression(compilation, type);
-    }
-
-    private static InvocationExpressionSyntax GetPrivateType(Compilation compilation, INamedTypeSymbol type)
-    {
-        var expression = InvocationExpression(
-                MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, GetPrivateAssembly(type.ContainingAssembly), IdentifierName("GetType"))
-            )
-           .WithArgumentList(
-                ArgumentList(
-                    SingletonSeparatedList(Argument(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal(Helpers.GetFullMetadataName(type)))))
-                )
-            );
-
-        return type.IsGenericType && !type.IsOpenGenericType()
-            ? InvocationExpression(MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, expression, IdentifierName("MakeGenericType")))
-               .WithArgumentList(
-                    // ReSharper disable once NullableWarningSuppressionIsUsed
-                    ArgumentList(SeparatedList(type.TypeArguments.Select(t => Argument(GetTypeOfExpression(compilation, ( t as INamedTypeSymbol )!)))))
-                )
-            : expression;
     }
 
     private static readonly Regex SpecialCharacterRemover = new("[^\\w\\d]", RegexOptions.Compiled);
